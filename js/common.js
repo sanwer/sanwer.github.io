278ddbd055737e2a13ed674ddab901e6111d9564
sanwer <sanwer@qq.com>
2018-04-19 19:34:51 +0800

    Sanwer.com

---
+++ js/common.js
@@ -0,0 +1,8 @@
+/* Baidu Statistical */
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?8bf601f5bf00946916771069c85798be";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
